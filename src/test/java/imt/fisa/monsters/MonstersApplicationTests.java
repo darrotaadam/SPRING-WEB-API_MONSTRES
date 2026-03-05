5e2de05f2b300b56ac0fd8136fbc076d812867fb
darrotaadam <adam.darrot@etu.imt-nord-europe.fr>
2026-03-05 13:29:12 +0100

    add : base spring project

---
+++ src/test/java/imt/fisa/monsters/MonstersApplicationTests.java
@@ -0,0 +1,13 @@
+package imt.fisa.monsters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonstersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
